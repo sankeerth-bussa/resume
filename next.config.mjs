e1036a21bb971ebf9d76fdb559eafa190c9da038
Sankeerth Bussa <sankeerthbussa@gmail.com>
2024-09-07 17:20:55 +0530

    Refactor Next.js build configuration for GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 // next.config.mjs
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: isProd ? "/resume" : "",
-  assetPrefix: isProd ? "/resume/" : "",
+  basePath: isProd ? '/resume' : '',
+  assetPrefix: isProd ? '/resume/' : '',
   reactStrictMode: true,
-  output: "export",
+  output: 'export',
   images: {
     unoptimized: true,
   },
